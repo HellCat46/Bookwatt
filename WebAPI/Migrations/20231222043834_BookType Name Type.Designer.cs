8a4af0afca5b5d189ed2fe180cd6a76120bdff2a
HellCat46 <hellcat2388@gmail.com>
2023-12-22 10:23:55 +0530

    Updates DB Models

--- WebAPI/Migrations/20231222043834_BookType Name Type.Designer.cs
+++ WebAPI/Migrations/20231222043834_BookType Name Type.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAPI.dbContext;
 
@@ -11,9 +12,11 @@ using WebAPI.dbContext;
 namespace WebAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231222043834_BookType Name Type")]
+    partial class BookTypeNameType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
